df8b2e2ad735b5dd4cfaf346b1b52a3b9066d113
karthik-1310 <karthikcosmic1310@gmail.com>
2024-11-03 23:28:04 +0530

    providers organised

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,6 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:reddit_clone/core/providers/firebase_providers.dart';
+
+final authRepositoryProvider = Provider(
+  (ref) => AuthRepository(
+    firestore: ref.read(firestoreProvider),
+    auth: ref.read(authProvider),
+    googleSignIn: ref.read(googleSignInProvider),
+  ),
+);
 
 class AuthRepository {
   final FirebaseFirestore _firestore;
@@ -13,4 +23,16 @@ class AuthRepository {
   })  : _auth = auth,
         _firestore = firestore,
         _googleSignIn = googleSignIn;
+
+  void signInwithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final credential = GoogleAuthProvider.credential(
+          accessToken: (await googleUser?.authentication)?.accessToken,
+          idToken: (await googleUser?.authentication)?.idToken);
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+      print(userCredential.user?.email);
+    } catch (e) {}
+  }
 }
